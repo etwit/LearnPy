e5c38a279c8b690af2390c9ae5f1cb5a065ca0f8
etwit <zznhaveadream@163.com>
2017-09-25 18:02:21 +0800

    day7

---
+++ Day7/ex13select代码.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#_*_coding:utf-8_*_
+'''
+Created on 2017年9月25日
+
+@author: Ethan Wong
+'''
+
+
+import select
+import socket
+import sys
+import Queue
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.setblocking(0)
+
+server_address = ('127.0.0.1',10000)
+print >>sys.stderr,'starting up on %s port %s'%server_address
+server.bind(server_address)
+
+server.listen(5)
+
+# Sockets from which we expect to read
+inputs = [ server ]
+ 
+# Sockets to which we expect to write
+outputs = [ ]
+ 
+message_queues = {}
+while inputs:
+ 
+    # Wait for at least one of the sockets to be ready for processing
+    print( '\nwaiting for the next event')
+    readable, writable, exceptional = select.select(inputs, outputs, inputs)
+    # Handle inputs
+    for s in readable:
+ 
+        if s is server:
+            # A "readable" server socket is ready to accept a connection
+            connection, client_address = s.accept()
+            print('new connection from', client_address)
+            connection.setblocking(False)
+            inputs.append(connection)
+ 
+            # Give the connection a queue for data we want to send
+            message_queues[connection] = Queue.Queue()
+        else:
+            data = s.recv(1024)
+            if data:
+                # A readable client socket has data
+                print(sys.stderr, 'received "%s" from %s' % (data, s.getpeername()) )
+                message_queues[s].put(data)
+                # Add output channel for response
+                if s not in outputs:
+                    outputs.append(s)
+            else:
+                # Interpret empty result as closed connection
+                print('closing', client_address, 'after reading no data')
+                # Stop listening for input on the connection
+                if s in outputs:
+                    outputs.remove(s)  #既然客户端都断开了，我就不用再给它返回数据了，所以这时候如果这个客户端的连接对象还在outputs列表中，就把它删掉
+                inputs.remove(s)    #inputs中也删除掉
+                s.close()           #把这个连接关闭掉
+ 
+                # Remove message queue
+                del message_queues[s]
+    # Handle outputs
+    for s in writable:
+        try:
+            next_msg = message_queues[s].get_nowait()
+        except Queue.Empty:
+            # No messages waiting so stop checking for writability.
+            print('output queue for', s.getpeername(), 'is empty')
+            outputs.remove(s)
+        else:
+            print( 'sending "%s" to %s' % (next_msg, s.getpeername()))
+            s.send(next_msg)
+    # Handle "exceptional conditions"
+    for s in exceptional:
+        print('handling exceptional condition for', s.getpeername() )
+        # Stop listening for input on the connection
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+ 
+        #Remove message queue
+        del message_queues[s]
---
+++ Day7/ex14.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#_*_coding:utf-8_*_
+'''
+Created on 2017年9月25日
+
+@author: Ethan Wong
+'''
+#客户端在命令窗口中执行
+import socket
+import sys
+ 
+messages = [ 'This is the message. ',
+             'It will be sent ',
+             'in parts.',
+             ]
+server_address = ('127.0.0.1', 10000)
+ 
+# Create a TCP/IP socket
+socks = [ socket.socket(socket.AF_INET, socket.SOCK_STREAM),
+          socket.socket(socket.AF_INET, socket.SOCK_STREAM),
+          ]
+ 
+# Connect the socket to the port where the server is listening
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+for s in socks:
+    s.connect(server_address)
+ 
+for message in messages:
+ 
+    # Send messages on both sockets
+    for s in socks:
+        print >>sys.stderr, '%s: sending "%s"' % (s.getsockname(), message)
+        s.send(message)
+ 
+    # Read responses on both sockets
+    for s in socks:
+        data = s.recv(1024)
+        print >>sys.stderr, '%s: received "%s"' % (s.getsockname(), data)
+        if not data:
+            print >>sys.stderr, 'closing socket', s.getsockname()
+            s.close()
